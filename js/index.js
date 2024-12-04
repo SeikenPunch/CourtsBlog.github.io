e584b20e6aadd4a6ff87ac7145b8252cbdf93fd3
SeikenPunch <jamluceononuro@gmail.com>
2024-12-04 15:55:46 -0800

    Finished Final. Still need to finish other pages

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+const hamMenu = document.querySelector('.hamMenu');
+
+const offScreenMenu = document.querySelector('.offScreenMenu');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+})
